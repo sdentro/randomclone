a6a22a8888fdc66a7f6976694858d5edca666330
Stefan Dentro <sdentro@gmail.com>
2018-02-13 13:59:06 +0000

    Adding update to cluster sizes

--- randomclone_informed_wrapup.R
+++ randomclone_informed_wrapup.R
@@ -4,15 +4,19 @@
 args = commandArgs(T)
 summary_table_mtimer_file = args[1]
 summary_table_randomclone_file = args[2]
-samplename = args[3]
-outdir = args[4]
+subclonal_structure_file = args[3]
+samplename = args[4]
+outdir = args[5]
 
 summ = readr::read_tsv(summary_table_mtimer_file)
 anno = readr::read_tsv(summary_table_randomclone_file)
 unlink(summary_table_mtimer_file, summary_table_randomclone_file)
 
+structure = readr::read_tsv(subclonal_structure_file)
+
 anno$num_clonal = summ$num_clonal
 anno$num_subclonal = summ$num_subclonal
 anno$frac_clonal = summ$frac_clonal
+anno$cluster_sizes = paste(structure$n_snvs, collapse=";")
 anno = anno[, (!grepl("samplename.1", colnames(anno)))]
 write.table(anno, file.path(outdir, paste0(samplename, "_summary_table.txt")), quote=F, sep="\t", row.names=F)
